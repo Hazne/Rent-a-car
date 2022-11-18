358f58e0f0da02c8991771724b8a796c751c6e45
Adnan Haznadarevic <adnan.haznadarevic@thefootballcompany.com>
2022-11-18 18:18:45 +0100

    Add Api (Update, Delete) for Employees

--- RentAPI/Controllers/EmployeesController.cs
+++ RentAPI/Controllers/EmployeesController.cs
@@ -37,5 +37,55 @@ namespace RentAPI.Controllers
 
             return Ok(employeeRequest);
         }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> GetEmployee([FromRoute] Guid id)
+        {
+            var employee = await _applicationDbContext.Employees.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (employee == null)
+                return NotFound();
+
+            return Ok(employee);
+        }
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> UpdateEmployee([FromRoute] Guid id, Employee updateEmployeeRequest)
+        {
+            var employee =  await _applicationDbContext.Employees.FindAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            employee.Name = updateEmployeeRequest.Name;
+            employee.Email = updateEmployeeRequest.Email;
+            employee.Salary = updateEmployeeRequest.Salary;
+            employee.Phone = updateEmployeeRequest.Phone;
+            employee.Deparment = updateEmployeeRequest.Deparment;
+
+            await _applicationDbContext.SaveChangesAsync();
+            return Ok(employee);
+        }
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> DeleteEmployee([FromRoute] Guid id)
+        {
+            var employee = await _applicationDbContext.Employees.FindAsync(id);
+
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            _applicationDbContext.Employees.Remove(employee);
+            await _applicationDbContext.SaveChangesAsync();
+            return Ok(employee);
+        }
     }
 }
